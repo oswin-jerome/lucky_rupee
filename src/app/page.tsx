0b13f7ad6bd0c9baee2869c65313a607e695d88e
oswin-jerome <oswinjeromej@gmail.com>
2024-06-01 11:33:59 +0530

    Clean

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,18 +2,6 @@ import { DB_ID, database } from "@/utils/appWrite";
 import { Button, Text } from "@radix-ui/themes";
 import { Models } from "appwrite";
 
-type Name = Models.Document & {
-  name?: string;
-};
-
 export default async function Home() {
-  const data: Name[] = (await database.listDocuments(DB_ID, "names")).documents;
-
-  return (
-    <main className="">
-      {data.map((e) => {
-        return <div>{e.name}</div>;
-      })}
-    </main>
-  );
+  return <main className="">Hello</main>;
 }
