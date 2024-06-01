db6daf01dd7c86ff9b6a7ee49f57b08491da92c1
oswin-jerome <oswinjeromej@gmail.com>
2024-06-01 11:32:10 +0530

    Init AppWrite

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,19 @@
+import { DB_ID, database } from "@/utils/appWrite";
 import { Button, Text } from "@radix-ui/themes";
+import { Models } from "appwrite";
 
-export default function Home() {
+type Name = Models.Document & {
+  name?: string;
+};
+
+export default async function Home() {
+  const data: Name[] = (await database.listDocuments(DB_ID, "names")).documents;
+
   return (
     <main className="">
-      <Text>Hello from Radix Themes :)</Text>
-      <Button>Let's go</Button>
+      {data.map((e) => {
+        return <div>{e.name}</div>;
+      })}
     </main>
   );
 }
---
+++ src/utils/appWrite.ts
@@ -0,0 +1,8 @@
+import { Client, Databases } from "appwrite";
+export const client = new Client();
+
+client.setEndpoint("http://localhost:81/v1").setProject("665ab5f500104dde5b6a");
+
+export const database = new Databases(client);
+
+export const DB_ID = "665ab790000e4b711df6";
